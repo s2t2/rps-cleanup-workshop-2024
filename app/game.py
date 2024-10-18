9a4f6294fb7803bf581024edb36979db87714456
MJ Rossetti <s2t2@users.noreply.github.com>
2024-10-18 13:54:23 -0400

    Refactor winner determination logic using custom function

--- app/game.py
+++ app/game.py
@@ -25,21 +25,33 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("TIE GAME")
-elif u == "rock" and c == "paper":
-    print("COMPUTER WINS")
-elif u == "rock" and c == "scissors":
-    print("USER WINS")
-elif u == "paper" and c == "rock":
-    print("COMPUTER WINS") # OOPS
-elif u == "paper" and c == "paper":
-    print("TIE GAME")
-elif u == "paper" and c == "scissors":
-    print("USER WINS") # OOPS
-elif u == "scissors" and c == "rock":
-    print("COMPUTER WINS")
-elif u == "scissors" and c == "paper":
-    print("USER WINS")
-elif u == "scissors" and c == "scissors":
-    print("TIE GAME")
+def determine_outcome(u:str, c:str):
+    """Determines the winner in a game of R, P, S.
+
+    Params :
+        u (str): user choice - one of the valid options ("rock", "paper", "scissors")
+        c (str): computer choice - one of the valid options ("rock", "paper", "scissors")
+    """
+
+    if u == "rock" and c == "rock":
+        return "TIE GAME"
+    elif u == "rock" and c == "paper":
+        return "COMPUTER WINS"
+    elif u == "rock" and c == "scissors":
+        return "USER WINS"
+    elif u == "paper" and c == "rock":
+        return "USER WINS"
+    elif u == "paper" and c == "paper":
+        return "TIE GAME"
+    elif u == "paper" and c == "scissors":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "rock":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "paper":
+        return "USER WINS"
+    elif u == "scissors" and c == "scissors":
+        return "TIE GAME"
+
+
+outcome = determine_outcome(u, c)
+print(outcome)
